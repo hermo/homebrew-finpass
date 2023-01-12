38d651cbca9bc5d4fd02e2fc110288ab1173d360
goreleaserbot <mirko+goreleaser@mirko.fi>
2023-01-12 19:28:07 +0200

    Brew formula update for finpass version v1.3.0

--- Formula/finpass.rb
+++ Formula/finpass.rb
@@ -5,21 +5,21 @@
 class Finpass < Formula
   desc "finpass generates passwords using Finnish language words"
   homepage "https://github.com/hermo/finpass"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hermo/finpass/releases/download/v1.2.0/finpass_1.2.0_macOS_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "5aba1fd810df3d493e3096a1311bf59d5f6e3f90998986901e50e3d6f6aed7b7"
+      url "https://github.com/hermo/finpass/releases/download/v1.3.0/finpass_1.3.0_macOS_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "fcfa0f6bf2c67996e8302e0847d9ff334fc1af350df2c51bab017fa58d856bc7"
 
       def install
         bin.install "finpass"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/hermo/finpass/releases/download/v1.2.0/finpass_1.2.0_macOS_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "2669d2553e2b3c26b7b55b6d90c2bdf89451f560cfa4c9a2ab4a726f3a89fc20"
+      url "https://github.com/hermo/finpass/releases/download/v1.3.0/finpass_1.3.0_macOS_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "095e4bcf6ec87f881752dcef729130781868253f16ecd490abb87db31989e388"
 
       def install
         bin.install "finpass"
@@ -29,8 +29,8 @@ class Finpass < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/hermo/finpass/releases/download/v1.2.0/finpass_1.2.0_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "8a7eff27a54d1e2a26072d87d388dfaca5597b529cd421cfdf8324409eeecdcf"
+      url "https://github.com/hermo/finpass/releases/download/v1.3.0/finpass_1.3.0_linux_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "003895eaab78c2b71a297b7460fde91738adb57ca97900fb5bc9bbf49dfcc09a"
 
       def install
         bin.install "finpass"
