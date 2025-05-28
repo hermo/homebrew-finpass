20a09d13562d19b1254275ccdbbd89810c8e32d4
goreleaserbot <mirko+goreleaser@mirko.fi>
2025-05-28 09:21:25 +0000

    Brew formula update for finpass version v1.4.2

--- Formula/finpass.rb
+++ Formula/finpass.rb
@@ -5,21 +5,21 @@
 class Finpass < Formula
   desc "finpass generates passwords using Finnish language words"
   homepage "https://github.com/hermo/finpass"
-  version "1.4.1"
+  version "1.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hermo/finpass/releases/download/v1.4.1/finpass_1.4.1_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "0684d044312d98f303b32b0f8081700e19895bf2cf442596476884644a68895a"
+      url "https://github.com/hermo/finpass/releases/download/v1.4.2/finpass_1.4.2_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "f1952dca803d8c0bfa54f1b60eca95d42eae89361ad5495d9c80c059e723aee3"
 
       def install
         bin.install "finpass"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/hermo/finpass/releases/download/v1.4.1/finpass_1.4.1_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "c6bcc742d6a69b2f8dcacbfce1068cf4987d6473b83713365646021cecc2dd71"
+      url "https://github.com/hermo/finpass/releases/download/v1.4.2/finpass_1.4.2_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "9beb34a08da2fa1e17b4bc48792f9c5d18d857b0e7a3313eeac4b5700f4c0721"
 
       def install
         bin.install "finpass"
@@ -29,8 +29,8 @@ class Finpass < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/hermo/finpass/releases/download/v1.4.1/finpass_1.4.1_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "e0f9abf5d3f415dc4195e1fd61b0fa665c2e72e7d3f3d02267ee6ce3ff99b6fd"
+      url "https://github.com/hermo/finpass/releases/download/v1.4.2/finpass_1.4.2_linux_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "3109c2c0d98c6a332f00bce848b545c3a4c624483ed9bab59d1c2259d797c080"
       def install
         bin.install "finpass"
       end
